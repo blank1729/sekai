73cc12485008a6c27516fafea8195779b10b521f
blank <85779539+blank1729@users.noreply.github.com>
2023-06-06 04:12:39 +0530

    basic setup #4

--- api/products.go
+++ api/products.go
@@ -15,7 +15,7 @@ var Product struct {
 
 func ProductsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		item, err := os.Open("data/item.json")
+		item, err := os.Open("data/items.json")
 		if err != nil {
 			fmt.Fprintln(w, err)
 		}
